b62e0fe830b8f5e39788315c4f1b638631a18d6b
Davi Inácio <aazz6850@gmail.com>
2020-12-27 20:09:41 -0300

    Logic refactor Medium difficult implemented

---
+++ app/src/main/java/com/daviapps/tictactoe/core/MachineHard.java
@@ -0,0 +1,11 @@
+package com.daviapps.tictactoe.core;
+
+import com.daviapps.tictactoe.domain.Position;
+
+// Created by daviinacio on 27/12/2020.
+public class MachineHard extends MachineMedium {
+    @Override
+    public Position choose(Game game) {
+        return super.choose(game);
+    }
+}
